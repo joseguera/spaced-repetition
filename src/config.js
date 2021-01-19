d121028ff8f66575e3966ca955547e68ed99083e
Joseguera <joseguera@uclaextension.edu>
2021-01-18 21:54:20 -0800

    updated api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://jose-spaced-repetition.herokuapp.com/api/',
+  API_ENDPOINT: 'https://jose-spaced-repetition.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
