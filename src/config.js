b2828a038634832e71f74d0d4d339c40427eb69c
Joseguera <joseguera@uclaextension.edu>
2021-01-19 11:43:51 -0800

    redeploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   API_ENDPOINT: 'https://jose-spaced-repetition.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
